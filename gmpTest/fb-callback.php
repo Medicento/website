8266168c2f0f64739bf731ced90ff0f86446285b
Prashant Bhardwaj <pkpbhardwaj729@gmail.com>
2018-01-10 13:38:54 +0530

    temp dir1

---
+++ gmpTest/fb-callback.php
@@ -0,0 +1,46 @@
+<?php
+    require_once __DIR__ . '/vendor/autoload.php'; // change path as needed
+    $fb = new Facebook\Facebook([
+        'app_id' => '167395840543313',
+        'app_secret' => 'a72682c426f0d4f01a018a15987955c8',
+        'default_graph_version' => 'v2.10',
+    ]);
+   
+    $helper = $fb->getRedirectLoginHelper();
+   
+    try {
+        $accessToken = $helper->getAccessToken();
+    } catch(Facebook\Exceptions\FacebookResponseException $e) {
+        // When Graph returns an error
+        echo 'Graph returned an error: ' . $e->getMessage();
+        exit;
+    } catch(Facebook\Exceptions\FacebookSDKException $e) {
+        // When validation fails or other local issues
+        echo 'Facebook SDK returned an error: ' . $e->getMessage();
+        exit;
+    }
+   
+    if (! isset($accessToken)) {
+        if ($helper->getError()) {
+            header('HTTP/1.0 401 Unauthorized');
+            echo "Error: " . $helper->getError() . "\n";
+            echo "Error Code: " . $helper->getErrorCode() . "\n";
+            echo "Error Reason: " . $helper->getErrorReason() . "\n";
+            echo "Error Description: " . $helper->getErrorDescription() . "\n";
+        } else {
+            header('HTTP/1.0 400 Bad Request');
+            echo 'Bad request';
+        }
+        exit;
+    }
+   
+    // Logged in
+    // The OAuth 2.0 client handler helps us manage access tokens
+    $oAuth2Client = $fb->getOAuth2Client();
+   
+    // Get the access token metadata from /debug_token
+    $tokenMetadata = $oAuth2Client->debugToken($accessToken);
+   
+    // Get user’s Facebook ID
+    $userId = $tokenMetadata->getField('user_id');
+?>
