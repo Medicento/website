5c32050dde12c28c691e2d7a2e6bcf236a1f53ae
Prashant Bhardwaj <pkpbhardwaj729@gmail.com>
2018-01-10 13:52:01 +0530

    temp dir2

--- gmpTest/fb-callback.php
+++ gmpTest/fb-callback.php
@@ -1,5 +1,9 @@
 <?php
     require_once __DIR__ . '/vendor/autoload.php'; // change path as needed
+
+    if (!session_id()) {
+        session_start();
+    }
     $fb = new Facebook\Facebook([
         'app_id' => '167395840543313',
         'app_secret' => 'a72682c426f0d4f01a018a15987955c8',
